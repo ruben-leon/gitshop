84a233fb4c98bf2473e060fa59a5a163df4eb2e0
RubenKanibal <rubenleon@web.de>
2023-05-31 00:46:52 +0200

    Add data.js to html

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,39 @@
 import { stringifyGuitars } from "./data.js";
 const parsedGuitars = JSON.parse(stringifyGuitars);
+console.log(Object.entries(parsedGuitars));
+
+
+Object.entries(parsedGuitars).forEach((guitarType) => {
+
+    console.log(guitarType);
+
+    const sections = [
+        document.querySelector("#acoustic"),
+        document.querySelector("#electric"),
+    ];
+
+    guitarType[1].map((card) => {
+
+        console.log(card);
+        sections.forEach((section) => {
+            if (card.type === section.id) {
+                let cardInfo = `
+                <div style="display: flex; justify-content: space-evenly; align-items: center; 
+                flex-direction: row">
+                <div class="card-image" style="border: 1px white solid">
+                    <div style="width: 24rem;
+                    height: 18rem;
+                    background-image: url('${card.image}'); 
+                    background-position: center; background-size: cover;">
+                    </div>
+                </div>
+                </div>
+                `
+                section.insertAdjacentHTML("afterbegin", cardInfo);
+            }
+        });
+    });
+});
+
+
+
